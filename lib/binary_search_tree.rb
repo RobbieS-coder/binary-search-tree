d59b9571e190b52c4f15ff423ae296a5e612699f
RobbieS-coder <127651345+RobbieS-coder@users.noreply.github.com>
2024-02-13 16:48:20 +0000

    Add insert method

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -26,9 +26,29 @@ class Tree
     root
   end
 
+  def insert(value, node = @root)
+    return nil if contains?(value)
+
+    return Node.new(value) if node.nil?
+
+    node.left = insert(value, node.left) if value < node.value
+    node.right = insert(value, node.right) if value > node.value
+
+    node
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  private
+
+  def contains?(value, node = @root)
+    return false if node.nil?
+    return true if value == node.value
+
+    contains?(value, node.left) || contains?(value, node.right)
   end
 end
