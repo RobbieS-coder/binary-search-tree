809677ed72aef246819d20b4d3c55ec1ad5d40a7
RobbieS-coder <127651345+RobbieS-coder@users.noreply.github.com>
2024-02-13 15:41:31 +0000

    Assign balanced tree to @root when initialising

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,15 +1,34 @@
 class Node
   attr_accessor :value, :left, :right
 
-  def initialize(value, left, right)
+  def initialize(value)
     @value = value
-    @left = left
-    @right = right
+    @left = nil
+    @right = nil
   end
 end
 
 class Tree
   def initialize(arr)
-    @root = build_tree(arr.uniq)
+    @root = build_tree(arr.uniq.sort)
+  end
+
+  def build_tree(arr)
+    return nil if arr.empty?
+    return Node.new(arr[0]) if arr.length == 1
+
+    mid = arr.length / 2
+    root = Node.new(arr[mid])
+
+    root.left = build_tree(arr[0...mid])
+    root.right = build_tree(arr[mid + 1..-1])
+
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
